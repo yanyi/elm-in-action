60ad30551992391e15b4bf28c7ccc64c81799909
Yan Yi Goh <yy.git@protonmail.com>
2021-01-14 18:16:00 +0800

    Ch 2: Render selected thumbnail + using record in model

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -4,16 +4,47 @@ import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
 
 
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
+            (List.map
+                (\photo -> viewThumbnail model.selectedUrl photo)
+                model.photos
+            )
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
             ]
+            []
         ]
 
 
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+
+        {- classList has list of tuples, with each tuple containing the desired class name and then the Boolean check
+           indicate whether we want the class to be added to the final class string.
+        -}
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        ]
+        []
+
+
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
+
+
 main =
-    view "no model yet"
+    view initialModel
